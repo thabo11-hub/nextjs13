92cdb1472f6e2ff56c28c1de9816b6bc857315cb
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 21:39:55 +0200

    search params

--- app/search/[searcTerm]/page.tsx
+++ app/search/[searcTerm]/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
-function SearchResult() {
-  return (
+type PageProps = {
+    params:{
+        searchTerm: string;
+    };
+};
+
+const search = async (searchTerm: string) => {
+    const res = await fetch(`https://searchapi.com/search.json?q=${searchTerm}&api_key=${
+        process.env.API_KEY}`)
+    const data = await res.json();
+    return data;
+}
+
+async function SearchResult({params: {searchTerm}} : PageProps) {
+  const searchResults = await search(searchTerm);
+  
+    return (
     <div>SearchResult</div>
   )
 }
