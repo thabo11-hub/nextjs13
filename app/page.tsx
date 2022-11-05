5351aa1328aad70b3bebde0c0f9ef39402f189d6
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 22:52:37 +0200

    {/*@ts-ignore*/}

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ function Home() {
     <>
       <h1>Loading Todos</h1>
       <div className='flex space-x-2'>
+        {/*@ts-ignore*/}
         <TodoList />
       </div>
 
       <h1>Loading Shopping Trolley</h1>
       <div className='flex space-x-2'>
+        {/*@ts-ignore*/}
         <TodoList />
       </div>
     </>
