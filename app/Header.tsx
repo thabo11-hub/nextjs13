d624191300710e97a341d865f2de2312e976f193
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 14:19:23 +0200

    server components

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,8 +5,7 @@ function Header() {
     return (
         <header className='p-5 bg-blue-400'>
 
-            <Link href='/' className='px-2 py-1 bg-white text-blue-500
-            rounded-lg'>Home</Link>
+            <Link href='/' className='px-2 py-1 bg-white text-blue-500 rounded-lg'>Home</Link>
         </header>
     );
 }
