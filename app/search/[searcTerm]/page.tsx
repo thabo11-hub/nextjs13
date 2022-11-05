378a2e4ebaa4f3e3efa33e68be49e9ed016af815
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 21:59:15 +0200

    you search for

--- app/search/[searcTerm]/page.tsx
+++ app/search/[searcTerm]/page.tsx
@@ -20,7 +20,7 @@ type SearchResult = {
 
 const search = async (searchTerm: string) => {
     const res = await fetch(`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`)
-    const data:SearchResult = await res.json();
+    const data: SearchResult = await res.json();
     return data;
 }
 
@@ -28,8 +28,19 @@ async function SearchResult({ params: { searchTerm } }: PageProps) {
     const searchResults = await search(searchTerm);
 
     return (
-        <div>SearchResult</div>
-    )
+        <div>
+            <p className='text-gray-500 text-sm'>You search for: {searchTerm}</p>
+
+            <ol className='space-y-5 p-5'>
+                {searchResults.orgarnic_results.map((result) => (
+                    <li key={result.position} className='list-decimal'>
+                        <p className='font-bold'>{result.title}</p>
+                        <p>{result.snippet}</p>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 }
 
 export default SearchResult
