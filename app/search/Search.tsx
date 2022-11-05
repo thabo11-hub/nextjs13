2a7e1bf12c7b23a7141a033b8d43e21de719087c
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 20:32:45 +0200

    search functionality

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React, { FormEvent, useState } from 'react';
 
 function Search() {
-  return (
-    <div>Search Here........</div>
-  )
+    const [search, setSearch] = useState("");
+    const router = useRouter();
+
+    const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault();
+        setSearch('');
+        router.push(`/search/${search}`);
+    };
+
+    return (
+        <form onSubmit={handleSearch}>
+            <input type='text'
+                value={search}
+                placeholder='Enter search '
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type='submit' className='btn'>Search</button>
+        </form>
+    )
 }
 
-export default Search
+export default Search;
