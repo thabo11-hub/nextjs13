9b0a068de9f6d5b3e1c82724dbdbfc41b46dfd90
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 22:57:22 +0200

    Fallback

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TodoList from './todos/TodoList'
 function Home() {
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<p>Loading Todos.....</p>}>
         <h1>Loading Todos</h1>
         <div className='flex space-x-2'>
           {/*@ts-ignore*/}
@@ -12,7 +12,7 @@ function Home() {
         </div>
       </Suspense>
 
-      <Suspense>
+      <Suspense fallback={<p>Loading Shopping Trolley.....</p>}>
 
         <h1>Loading Shopping Trolley</h1>
         <div className='flex space-x-2'>
