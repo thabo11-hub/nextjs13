ee35e4e51534ed5c8ada0f66e91dde99466a6158
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 22:55:23 +0200

    Suspense both

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import React ,{Suspense}from 'react'
+import React, { Suspense } from 'react'
 import TodoList from './todos/TodoList'
 
 function Home() {
   return (
     <>
       <Suspense>
-
         <h1>Loading Todos</h1>
         <div className='flex space-x-2'>
           {/*@ts-ignore*/}
@@ -13,11 +12,14 @@ function Home() {
         </div>
       </Suspense>
 
-      <h1>Loading Shopping Trolley</h1>
-      <div className='flex space-x-2'>
-        {/*@ts-ignore*/}
-        <TodoList />
-      </div>
+      <Suspense>
+
+        <h1>Loading Shopping Trolley</h1>
+        <div className='flex space-x-2'>
+          {/*@ts-ignore*/}
+          <TodoList />
+        </div>
+      </Suspense>
     </>
   )
 }
