1aed9aab442b71df903c71598037e539c0414b6d
Thabo Sodi <thabosodi11@gmail.com>
2022-11-05 21:53:19 +0200

    search array results

--- app/search/[searcTerm]/page.tsx
+++ app/search/[searcTerm]/page.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
 
 type PageProps = {
-    params:{
+    params: {
         searchTerm: string;
     };
 };
 
+type SearchResult = {
+    orgarnic_results: [
+        {
+            position: number;
+            title: string;
+            link: string;
+            thumbnail: string;
+            snippet: string;
+        }
+    ];
+};
+
 const search = async (searchTerm: string) => {
-    const res = await fetch(`https://serpapi.com/search.json?q=${searchTerm}&api_key=${
-        process.env.API_KEY}`)
-    const data = await res.json();
+    const res = await fetch(`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`)
+    const data:SearchResult = await res.json();
     return data;
 }
 
-async function SearchResult({params: {searchTerm}} : PageProps) {
-  const searchResults = await search(searchTerm);
-  
+async function SearchResult({ params: { searchTerm } }: PageProps) {
+    const searchResults = await search(searchTerm);
+
     return (
-    <div>SearchResult</div>
-  )
+        <div>SearchResult</div>
+    )
 }
 
 export default SearchResult
